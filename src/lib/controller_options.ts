ec8dbc0606ec11dab62ee90a9680d8a881cec7aa
Lucas Palomo <devlpalomo@gmail.com>
2019-11-16 15:54:20 -0300

    arrumando valores esperados

--- src/lib/controller_options.ts
+++ src/lib/controller_options.ts
@@ -10,65 +10,61 @@ export class ControllerOptions {
 		this.res = res;
 	}
 	
-	valuesIsValid(expectedOrder: Map<string, Array<string>> | undefined): boolean {
+	valuesIsValid(
+		from:
+			{
+				params: Map<number, string>,
+				body: Map<number, string>,
+				query: Map<number, string>,
+				locals: Map<number, string>
+			}): boolean {
 		
 		let params: boolean = false;
 		let body: boolean = false;
 		let query: boolean = false;
 		let locals: boolean = false;
 		
-		expectedOrder?.forEach((value, key) => {
-			if (key.startsWith("params")) {
-				if (Object.keys(this.req.params) === value) {
-					params = true;
-				}
-			}
-			if (key.startsWith("body")) {
-				if (Object.keys(this.req.body) === value) {
-					body = true;
-				}
-			}
-			if (key.startsWith("query")) {
-				if (Object.keys(this.req.query) === value) {
-					query = true;
-				}
-			}
-			if (key.startsWith("locals")) {
-				if (Object.keys(this.res.locals) === value) {
-					locals = true;
-				}
-			}
-		});
+		if (Array.from(from.params.values()) === Object.keys(this.req.params)) {
+			params = true;
+		}
+		if (Array.from(from.body.values()) === Object.keys(this.req.body)) {
+			body = true;
+		}
+		if (Array.from(from.query.values()) === Object.keys(this.req.query)) {
+			query = true;
+		}
+		if (Array.from(from.locals.values()) === Object.keys(this.res.locals)) {
+			locals = true;
+		}
+		
 		return locals && body && params && query;
 	}
 	
-	formattedValues(expectedOrder: Map<string, Array<string>> | undefined): any[] {
+	formattedValues(
+		from:
+			{
+				params: Map<number, string>,
+				body: Map<number, string>,
+				query: Map<number, string>,
+				locals: Map<number, string>
+			}): any[] {
 		
-		let formattedValues: any[] = [];
+		let values: Map<number, any> = new Map<number, any>();
 		
-		expectedOrder?.forEach((value, key) => {
-			if (key.startsWith("params")) {
-				for (let i = 0; i <= value.length; i++) {
-					formattedValues[i] = this.req.params[value[i]];
-				}
-			}
-			if (key.startsWith("body")) {
-				for (let i = 0; i <= value.length; i++) {
-					formattedValues[i] = this.req.body[value[i]];
-				}
-			}
-			if (key.startsWith("query")) {
-				for (let i = 0; i <= value.length; i++) {
-					formattedValues[i] = this.req.query[value[i]];
-				}
-			}
-			if (key.startsWith("locals")) {
-				for (let i = 0; i <= value.length; i++) {
-					formattedValues[i] = this.res.locals[value[i]];
-				}
-			}
+		from.params.forEach((value, key) => {
+			values.set(key, this.req.params[value]);
 		});
-		return formattedValues;
+		from.body.forEach((value, key) => {
+			values.set(key, this.req.body[value]);
+		});
+		from.query.forEach((value, key) => {
+			values.set(key, this.req.query[value]);
+		});
+		from.locals.forEach((value, key) => {
+			values.set(key, this.res.locals[value]);
+		});
+		
+		return Array.from(values.values());
 	}
 	
 }
